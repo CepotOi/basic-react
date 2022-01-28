d9f41231f1492f7d87baac0487ac1406ebbcd9b3
CepotOi <rizkihutama69@gmail.com>
2022-01-28 23:23:13 +0700

    create ul with react

--- index.js
+++ index.js
@@ -10,16 +10,22 @@
  ** With React
 */
 const root = document.querySelector('#root');
-const p1 = React.createElement('p', {
-  children: 'Paragraph1',
-  key: 'p1',
-});
-const p2 = React.createElement('p', {
-  children: 'Paragraph2',
-  key: 'p2',
-});
-const element = React.createElement(React.Fragment, {
-  children: [p1, p2],
-});
+const ul = React.createElement('ul', null, [
+  React.createElement('li', {
+    key: 'Apple'
+  }, 'Apple'),
+  React.createElement('li', {
+    key: 'Banana'
+  }, 'Banana'),
+  React.createElement('li', {
+    key: 'Orange'
+  }, 'Orange'),
+  React.createElement('li', {
+    key: 'Avocado'
+  }, 'Avocado'),
+  React.createElement('li', {
+    key: 'Grape'
+  }, 'Grape'),
+]);
 
-ReactDOM.render(element, root);
+ReactDOM.render(ul, root);
