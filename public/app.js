f1d37d78d7f0e80b3e93532d723d47fe2cd45c2c
CepotOi <rizkihutama69@gmail.com>
2022-02-02 16:15:36 +0700

    external css react

--- public/app.js
+++ public/app.js
@@ -10,16 +10,7 @@
  ** With React
  */
 const root = document.querySelector('#root');
-
-const tick = () => {
-  const element = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    style: {
-      width: '10em',
-      height: '10em',
-      backgroundColor: 'red'
-    }
-  }));
-  ReactDOM.render(element, root);
-};
-
-setInterval(tick, 1000);
+const element = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+  className: "box"
+}));
+ReactDOM.render(element, root);
--- src/app.js
+++ src/app.js
@@ -10,14 +10,10 @@
  ** With React
  */
 const root = document.querySelector('#root');
-const tick = () => {
-    const element = (
-        <>
-            <div style={{ width: '10em', height: '10em', backgroundColor: 'red' }}></div>
-        </>
-    );
+const element = (
+    <>
+        <div className='box'></div>
+    </>
+);
 
-    ReactDOM.render(element, root);
-};
-
-setInterval(tick, 1000);
+ReactDOM.render(element, root);
